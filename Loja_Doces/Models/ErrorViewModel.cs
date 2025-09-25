707b6629ca80c719492649bca2b1e0fac676b775
Heitor e Iza <heitorvieirafernandes12@gmail.com>
2025-09-25 13:44:01 -0300

    Criando o arquivo gitignore

---
+++ Loja_Doces/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Loja_Doces.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
